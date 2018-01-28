46e057c6225960f39d2e5ef9df6a482521325804
Phivos Stylianides <stphivos@gmail.com>
2018-01-28 14:46:07 +0200

    Add dashboard charts with hardcoded data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,6 +15,7 @@ angular.module('starter.controllers', [])
     alert('Received push notification: ' + title + '\n' + text + '\n' + JSON.stringify(payload));
   });
 
+  // Example
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
   $scope.data = [
@@ -32,7 +33,52 @@ angular.module('starter.controllers', [])
       [65, 59, 80, 81, 56, 55, 40]
     ];
   }, 1000);
-})
+
+  // Steps
+  $scope.steps = {
+    labels: ['Current', 'Remaining'],
+    data: [30, 70],
+    colors: ['#86adc2', '#d4d4d4'],
+    options: {
+      legend: {
+        display: true
+      }
+    }
+  };
+
+  // Heart-rate
+  $scope.hr = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    data: [
+      [110, 115, 120, 95, 130, 122, 118],
+      [100, 100, 100, 100, 100, 100, 100],
+      [120, 120, 120, 120, 120, 120, 120],
+    ],
+    series: ['Actual', 'Recommended Low', 'Recommended High'],
+    options: {
+      showLines: true
+    }
+  };
+
+  // BMI
+  $scope.bmi = {
+    labels: ['August', 'September', 'October', 'November', 'December', 'January'],
+    series: ['Actual'],
+    data: [30, 29, 27, 27, 25, 24],
+    colors: ['#f12d3a', '#faa54a', '#faa54a', '#faa54a', '#faa54a', '#3bb4af'],
+    options: {
+      scales: {
+        xAxes: [{
+          display: true,
+          ticks: {
+              min: 10,
+              max: 40
+          }
+      }]
+    }
+    }
+  };
+}) // 12 - 40
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called

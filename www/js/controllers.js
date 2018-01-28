ac4f2635e87f77f817a28f36518fd0d649a82d4a
Phivos Stylianides <stphivos@gmail.com>
2018-01-28 14:51:42 +0200

    Remove chart example code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,25 +15,6 @@ angular.module('starter.controllers', [])
     alert('Received push notification: ' + title + '\n' + text + '\n' + JSON.stringify(payload));
   });
 
-  // Example
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-  
-  // Simulate async data update
-  $timeout(function () {
-    $scope.data = [
-      [28, 48, 40, 19, 86, 27, 90],
-      [65, 59, 80, 81, 56, 55, 40]
-    ];
-  }, 1000);
-
   // Steps
   $scope.steps = {
     labels: ['Current', 'Remaining'],
